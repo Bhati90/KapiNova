10c3a40ed7a0c3dd94f832166f919c596b62d275
kishan bhati <bhatikaran634@gmail.com>
2024-03-22 14:07:03 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HeroParallax } from "./components/ui/hero-parallex";
+import { HeroParallaxDemo } from "./components/ui/hero";
+import Footer from "./footer";
+import AboutUs from "./AboutUs";
+import Work from "./work";
+import ImageSlider from "./imageSlider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src="hero.png" className="hero"></img> */}
+      <ImageSlider />
+      <a
+        href="https://wa.me/7340002501"
+        className="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
+      <Work />
+      {/* <AboutUs /> */}
+      <Footer />
     </div>
   );
 }
